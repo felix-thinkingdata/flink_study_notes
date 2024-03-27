21221adb8ab6c9956fea0579eb281f1d716c80e4
felix.Wang <felix@thinkingdata.cn>
2024-03-27 10:29:02 +0800

    feat: Add flink web ui dependency in local mode

--- src/main/java/cn/thinkingdata/chapter04/WordCountKafkaInStdOut.java
+++ src/main/java/cn/thinkingdata/chapter04/WordCountKafkaInStdOut.java
@@ -1,22 +1,16 @@
 package cn.thinkingdata.chapter04;
 
 import org.apache.flink.api.common.eventtime.WatermarkStrategy;
-import org.apache.flink.api.common.functions.FlatMapFunction;
-import org.apache.flink.api.common.serialization.DeserializationSchema;
 import org.apache.flink.api.common.serialization.SimpleStringSchema;
 import org.apache.flink.api.common.typeinfo.Types;
 import org.apache.flink.api.java.tuple.Tuple2;
 import org.apache.flink.connector.kafka.source.KafkaSource;
 import org.apache.flink.connector.kafka.source.enumerator.initializer.OffsetsInitializer;
 import org.apache.flink.streaming.api.datastream.DataStream;
-import org.apache.flink.streaming.api.datastream.DataStreamSource;
 import org.apache.flink.streaming.api.datastream.SingleOutputStreamOperator;
 import org.apache.flink.streaming.api.environment.StreamExecutionEnvironment;
 import org.apache.flink.streaming.api.windowing.time.Time;
-import org.apache.flink.streaming.connectors.kafka.FlinkKafkaConsumer;
 import org.apache.flink.util.Collector;
-
-import java.util.Properties;
 
 
 public class WordCountKafkaInStdOut {
