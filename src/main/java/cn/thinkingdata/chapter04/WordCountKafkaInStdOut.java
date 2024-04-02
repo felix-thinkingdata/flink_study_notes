2724aaece960c018a4ccdff4b739a8fd1dd18627
felix.Wang <felix@thinkingdata.cn>
2024-04-02 16:46:04 +0800

    feat: Add flink web ui dependency in local mode

--- src/main/java/cn/thinkingdata/chapter04/WordCountKafkaInStdOut.java
+++ src/main/java/cn/thinkingdata/chapter04/WordCountKafkaInStdOut.java
@@ -16,11 +16,10 @@ import org.apache.flink.util.Collector;
 public class WordCountKafkaInStdOut {
 
     public static void main(String[] args) throws Exception {
-
         // 设置Flink执行环境
         StreamExecutionEnvironment env =
                 StreamExecutionEnvironment.getExecutionEnvironment();
-
+        //设置kafka source
         KafkaSource<String> kafkaSource = KafkaSource.<String>builder()
                 .setBootstrapServers("ta1:9092,ta2:9092,ta3:9092")
                 .setTopics("Shakespeare")
@@ -29,17 +28,20 @@ public class WordCountKafkaInStdOut {
                 .setValueOnlyDeserializer(new SimpleStringSchema())
                 .build();
         DataStream<String> stream = env.fromSource(kafkaSource, WatermarkStrategy.noWatermarks(), "kafka source");
+        // 从Kafka中读取数据
         SingleOutputStreamOperator<Tuple2<String, Integer>> streamOperator = stream.flatMap((String value, Collector<Tuple2<String, Integer>> out) -> {
             String[] split = value.split(" ");
             for (String s : split) {
                 out.collect(Tuple2.of(s, 1));
             }
         });
+        // 计算单词出现的次数
         streamOperator.returns(Types.TUPLE(Types.STRING, Types.INT))
                 .keyBy(value -> value.f0)
                 .window(
                         org.apache.flink.streaming.api.windowing.assigners.TumblingProcessingTimeWindows.of(Time.seconds(5))
                 ).sum(1).print();
+        // 执行任务
         env.execute("count the number of times a word appears");
 
     }
